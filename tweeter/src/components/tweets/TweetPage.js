7f9f81db14c9a28a4672fc9180feb7ace28b9a93
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-12-15 21:39:00 +0100

    Empezar a usar tweetDetail con Redux

--- tweeter/src/components/tweets/TweetPage.js
+++ tweeter/src/components/tweets/TweetPage.js
@@ -2,31 +2,37 @@ import { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import Page from '../layout/Page';
+//Cambios en importaciones para tratr el tweetdetail con Redux
 import { getTweetDetail } from './service';
+import { getTweet } from '../../store/selectors';
+import { useSelector } from 'react-redux';
 
-const TweetPage = props => {
-  const [tweet, setTweet] = useState(null);
+const TweetPage = (props) => {
+  //const [tweet, setTweet] = useState(null);
   const { tweetId } = useParams();
   const navigate = useNavigate();
   const unmounteRef = useRef(false);
 
-  useEffect(() => {
-    getTweetDetail(tweetId)
-      .then(tweet => {
-        console.log('have response');
-        // if (unmounteRef.current) {
-        //   console.log('do nothing');
-        //   return;
-        // }
-        console.log('set state');
-        setTweet(tweet);
-      })
-      .catch(error => {
-        if (error.status === 404) {
-          navigate('404');
-        }
-      });
-  }, [tweetId, navigate]);
+  //Uso de redux para tratar los tweets
+  const tweet = useSelector((state) => getTweet(state, tweetId));
+
+  // useEffect(() => {
+  //   getTweetDetail(tweetId)
+  //     .then(tweet => {
+  //       console.log('have response');
+  //       // if (unmounteRef.current) {
+  //       //   console.log('do nothing');
+  //       //   return;
+  //       // }
+  //       console.log('set state');
+  //       setTweet(tweet);
+  //     })
+  //     .catch(error => {
+  //       if (error.status === 404) {
+  //         navigate('404');
+  //       }
+  //     });
+  // }, [tweetId, navigate]);
 
   useEffect(() => {
     return () => {
@@ -35,7 +41,9 @@ const TweetPage = props => {
   }, []);
 
   return (
-    <Page title="Tweet detail" {...props}>
+    <Page
+      title='Tweet detail'
+      {...props}>
       <div>{JSON.stringify(tweet)}</div>
     </Page>
   );
--- tweeter/src/store/selectors.js
+++ tweeter/src/store/selectors.js
@@ -1,1 +1,6 @@
 export const getIsLogged = (state) => state.auth;
+
+export const getTweets = (state) => state.tweets;
+
+export const getTweet = (state, tweetId) =>
+  getTweets(state).find((tweet) => tweet.id.toString() === tweetId);
